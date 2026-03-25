a1b9a1e3fb2c337000ae3c786c4e9d7e86ca636d
revdan <revdan@users.noreply.github.com>
2026-03-25 14:32:37 +0000

    chore: add dialyxir to deps and CI with PLT caching

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule FairPick.MixProject do
       version: "0.1.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [plt_file: {:no_warn, "priv/plts/project.plt"}]
     ]
   end
 
@@ -21,7 +22,8 @@ defmodule FairPick.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
